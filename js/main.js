7c148eac06503efcd6ed555af974d63a4e0742a9
NICDX <119955722+NICDX@users.noreply.github.com>
2023-07-25 01:32:00 +0300

    Отрисуй меня полностью

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
 import {createPhotoDescriptions} from './data.js';
+import {renderPicture} from './pictures.js';
 
-createPhotoDescriptions();
+/*createPhotoDescriptions();*/
+renderPicture (createPhotoDescriptions);
---
+++ js/pictures.js
@@ -0,0 +1,24 @@
+const tmp = document.querySelector('#picture').content.querySelector('.picture');
+const pictureBlock = document.querySelector('.pictures');
+
+const createPicture = ({id, url, description, comments, likes}) => {
+  const pictureTmp = tmp.cloneNode(true);
+  pictureTmp.dataset.pictureId = id || '';
+  pictureTmp.querySelector('.picture__img').src = url || '';
+  pictureTmp.querySelector('.picture__img').alt = description || '';
+  pictureTmp.querySelector('.picture__comments').textContent = comments.length || '';
+  pictureTmp.querySelector('.picture__likes').textContent = likes || '';
+  return pictureTmp;
+};
+
+const renderPicture = (pictures) => {
+  const fragment = document.createDocumentFragment();
+  pictures.forEach((picture) => {
+    fragment.appendChild(createPicture(picture));
+  });
+  pictureBlock.appendChild(fragment);
+};
+
+export {renderPicture};
+
+
